fb979b3c1e090c41cd013a129b75fe4cc01023cd
SampreethC <sampreeth.chelampalem@charitableimpact.com>
2021-08-04 13:03:15 +0530

    Completed

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.githubToken = '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';
+config.githubUsername = 'SampreethC'; // not secret just saves putting in a few places
+
+module.exports = config;
